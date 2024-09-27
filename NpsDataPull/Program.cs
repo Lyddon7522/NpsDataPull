534a3dbbc132eb27bcf9f62c6b254f56a325b136
mlyddon <mlyddon@agleader.com>
2024-09-27 15:33:42 -0500

    Added call to the api

--- NpsDataPull/Program.cs
+++ NpsDataPull/Program.cs
@@ -15,11 +15,33 @@ var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddSingleton(configuration);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
-//builder.Services.AddHttpClient<INpsParkService, NpsParkService>();
 
 var apiKey = configuration["ApiSettings:ApiKey"];
 using HttpClient httpClient = new();
 httpClient.DefaultRequestHeaders.Add("X-Api-Key", apiKey);
+
+builder.Services.AddSingleton<INpsParkService>(
+    sp => new NpsParkService(sp.GetRequiredService<IConfiguration>(), httpClient));
+
+var host = builder.Build();
+
+using var scope = host.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var npsParkService = services.GetRequiredService<INpsParkService>();
+    var parks = await npsParkService.GetParksAsync();
+    // Use the parks data here
+    Console.WriteLine($"Total Parks: {parks?.Total}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An error occurred while getting parks data.");
+    Console.WriteLine(ex.Message);
+}
+
+await host.RunAsync();
 
 
 
